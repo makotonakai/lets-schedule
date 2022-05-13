f1fc0319ce50cd709ab5cbaf11b7621a30478f00
MakotoNakai <macofandohr1812@gmail.com>
2022-05-13 21:00:34 +0900

    feat: Add the sign up page

---
+++ src/view/src/components/header/SignupHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="../../assets/logo.png" width="140" height="64">
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Log in</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
--- src/view/src/router/index.js
+++ src/view/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     },
     {
       path: '/login',
