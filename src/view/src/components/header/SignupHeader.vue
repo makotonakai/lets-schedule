755f88b6b5cc59af1ae46c3ea7d79d329e96c8eb
MakotoNakai <macofandohr1812@gmail.com>
2022-05-13 22:41:55 +0900

    fix: change default path from /home to /

--- src/view/src/components/header/SignupHeader.vue
+++ src/view/src/components/header/SignupHeader.vue
@@ -3,7 +3,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item">
-          <router-link to="/home">
+          <router-link to="/">
             <img src="../../assets/logo.png" width="140" height="64">
           </router-link>
         </a>
