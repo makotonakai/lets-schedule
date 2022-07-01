34023c69907b8905f55a9fce402eec8e208d11ec
MakotoNakai <macofandohr1812@gmail.com>
2022-07-02 00:32:39 +0900

    [fix] updated the initial data

--- build/db/initdb.d/init.sql
+++ build/db/initdb.d/init.sql
@@ -21,13 +21,17 @@ create table if not exists meetings (
   `type` varchar(191),
   `meeting_place` varchar(191),
   `meeting_url` varchar(191),
+  `is_determined` boolean not null,
   `created_at` timestamp not null default current_timestamp,
   `updated_at` timestamp not null default current_timestamp on update current_timestamp,
   primary key(id)
 );
 
-insert into meetings values (1, "AQUA meeting", "Just a weekly meeting", "meeting", "delta", "http://keio-zoom.com", current_timestamp, current_timestamp);
-insert into meetings values (2, "Meeting with Aram-san", "Progress report", "meeting", "", "http://gaiax-zoom.com", current_timestamp, current_timestamp);
+insert into meetings values (1, "meeting1", " description1", "hybrid", "place1", "http://meeting1-zoom.com", 1, current_timestamp, current_timestamp);
+insert into meetings values (2, "meeting2", "description2", "physical", "place2", "", 0, current_timestamp, current_timestamp);
+insert into meetings values (3, "meeting3", "description3", "virtual", "", "http://meeting3-zoom.com", 0, current_timestamp, current_timestamp);
+insert into meetings values (4, "meeting4", "description4", "hybrid", "place4", "http://meeting4-zoom.com", 0, current_timestamp, current_timestamp);
+
 
 create table if not exists participants (
   `id` bigint(11) not null auto_increment,
@@ -41,7 +45,11 @@ create table if not exists participants (
 );
 
 insert into participants values (1, 1, "makoto", 1, 1, current_timestamp, current_timestamp);
-insert into participants values (2, 1, "minoru", 0, 0, current_timestamp, current_timestamp);
+insert into participants values (2, 2, "makoto", 1, 1, current_timestamp, current_timestamp);
+insert into participants values (3, 3, "minoru", 1, 1, current_timestamp, current_timestamp);
+insert into participants values (4, 3, "makoto", 0, 1, current_timestamp, current_timestamp);
+insert into participants values (5, 3, "minoru", 1, 1, current_timestamp, current_timestamp);
+insert into participants values (6, 4, "makoto", 0, 0, current_timestamp, current_timestamp);
 
 
 create table if not exists candidate_times (
