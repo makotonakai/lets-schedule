d12b4d0c87dd9e262ab780495bf7230b0d8fdc3f
MakotoNakai <macofandohr1812@gmail.com>
2022-07-22 19:33:52 +0900

    [feat] change DB design

--- src/database/initdb.d/init.sql
+++ src/database/initdb.d/init.sql
@@ -13,7 +13,6 @@ create table if not exists users (
 insert into users values (1, "makoto", "makoto@email.com", "password", 1, 1, current_timestamp, current_timestamp);
 insert into users values (2, "minoru", "minoru@email.com", "password", 1, 1, current_timestamp, current_timestamp);
 
-
 create table if not exists meetings (
   `id` bigint(11) not null auto_increment,
   `title` varchar(191) not null,
@@ -33,32 +32,62 @@ insert into meetings values (3, "meeting3", "description3", "virtual", "", "http
 insert into meetings values (4, "meeting4", "description4", "hybrid", "place4", "http://meeting4-zoom.com", 0, current_timestamp, current_timestamp);
 insert into meetings values (5, "meeting5", "description5", "hybrid", "place5", "http://meeting5-zoom.com", 0, current_timestamp, current_timestamp);
 
-
-create table if not exists participants (
+create table if not exists user_meetings (
   `id` bigint(11) not null auto_increment,
-  `meeting_id` bigint(11) not null,
   `user_id` bigint(11) not null,
-  `is_host` boolean not null,
-  `has_responded` boolean not null,
+  `meeting_id` bigint(11) not null,
   `created_at` timestamp not null default current_timestamp,
   `updated_at` timestamp not null default current_timestamp on update current_timestamp,
   primary key(id)
 );
+insert into user_meetings values (1, 1, 1, current_timestamp, current_timestamp);
+insert into user_meetings values (2, 2, 1, current_timestamp, current_timestamp);
+insert into user_meetings values (3, 3, 2, current_timestamp, current_timestamp);
+insert into user_meetings values (4, 3, 1, current_timestamp, current_timestamp);
+insert into user_meetings values (5, 4, 2, current_timestamp, current_timestamp);
+insert into user_meetings values (6, 4, 1, current_timestamp, current_timestamp);
+insert into user_meetings values (7, 5, 2, current_timestamp, current_timestamp);
+insert into user_meetings values (8, 5, 1, current_timestamp, current_timestamp);
 
-insert into participants values (1, 1, 1, 1, 1, current_timestamp, current_timestamp);
-insert into participants values (2, 2, 1, 1, 1, current_timestamp, current_timestamp);
-insert into participants values (3, 3, 2, 1, 1, current_timestamp, current_timestamp);
-insert into participants values (4, 3, 1, 0, 1, current_timestamp, current_timestamp);
-insert into participants values (5, 4, 2, 1, 1, current_timestamp, current_timestamp);
-insert into participants values (6, 4, 1, 0, 1, current_timestamp, current_timestamp);
-insert into participants values (7, 5, 2, 0, 0, current_timestamp, current_timestamp);
-insert into participants values (8, 5, 1, 0, 0, current_timestamp, current_timestamp);
+
+create table if not exists participants (
+  `user_meeting_id` bigint(11) not null,
+  `is_host` boolean not null,
+  `has_responded` boolean not null,
+  `created_at` timestamp not null default current_timestamp,
+  `updated_at` timestamp not null default current_timestamp on update current_timestamp,
+  primary key(user_meeting_id)
+);
+
+insert into participants values (1, 1, 1, current_timestamp, current_timestamp);
+insert into participants values (2, 1, 1, current_timestamp, current_timestamp);
+insert into participants values (3, 1, 1, current_timestamp, current_timestamp);
+insert into participants values (4, 0, 1, current_timestamp, current_timestamp);
+insert into participants values (5, 1, 1, current_timestamp, current_timestamp);
+insert into participants values (6, 0, 1, current_timestamp, current_timestamp);
+insert into participants values (7, 0, 0, current_timestamp, current_timestamp);
+insert into participants values (8, 0, 0, current_timestamp, current_timestamp);
+
+create table if not exists user_times (
+  `user_meeting_id` bigint(11) not null, 
+  `candidate_time_id` bigint(11) not null ,
+  `created_at` timestamp not null default current_timestamp,
+  `updated_at` timestamp not null default current_timestamp on update current_timestamp,
+  primary key(user_meeting_id, candidate_time_id)
+);
+
+insert into user_times values (1, 1, current_timestamp, current_timestamp);
+insert into user_times values (2, 2, current_timestamp, current_timestamp);
+insert into user_times values (3, 3, current_timestamp, current_timestamp);
+insert into user_times values (4, 4, current_timestamp, current_timestamp);
+insert into user_times values (5, 5, current_timestamp, current_timestamp);
+insert into user_times values (6, 6, current_timestamp, current_timestamp);
+insert into user_times values (7, 7, current_timestamp, current_timestamp);
+insert into user_times values (8, 8, current_timestamp, current_timestamp);
 
 
 create table if not exists candidate_times (
   `id` bigint(11) not null auto_increment,
-  `meeting_id` bigint(11) not null,
-  `user_id` bigint(11) not null,
   `start_time` timestamp not null,
   `end_time` timestamp not null,
   `created_at` timestamp not null default current_timestamp,
@@ -66,13 +95,11 @@ create table if not exists candidate_times (
   primary key(id)
 );
 
-insert into candidate_times values (1, 1, 1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (2, 1, 2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (3, 2, 1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (4, 2, 2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (5, 3, 1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (6, 3, 2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (7, 4, 1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (8, 4, 2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (9, 5, 1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
-insert into candidate_times values (10, 5, 2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (1, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (2, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (3, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (4, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (5, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (6, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (7, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
+insert into candidate_times values (8, current_timestamp, current_timestamp, current_timestamp, current_timestamp);
