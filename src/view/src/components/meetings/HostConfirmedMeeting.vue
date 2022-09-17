35e499462dd62f9cd17e57855f4fb8d28be616f2
MakotoNakai <macofandohr1812@gmail.com>
2022-09-18 02:28:58 +0900

    [add] HostConfirmedMeeeting component

---
+++ src/view/src/components/meetings/HostConfirmedMeeting.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    title: String,
+    description: String,
+    type: String,
+    place: String,
+    url: String
+  }
+};
+</script>
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">{{ title }}</p>
+    </header>
+    <div class="card-content">
+      <div class="meeting-description">
+        概要: {{ description }} <br>
+        形式: {{ type }} <br>
+        開催場所: {{ place }} <br>
+        URL: {{ url }} <br>
+      </div>
+    </div>
+  </div>
+</template>
