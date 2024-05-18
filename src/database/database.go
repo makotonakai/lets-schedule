2c91046f57d4ae5d907876313fd90823a8f87a7e
makotonakai <macofandohr1812@gmail.com>
2024-05-19 00:46:17 +0900

    add logging

--- src/database/database.go
+++ src/database/database.go
@@ -6,6 +6,8 @@ import (
   "gorm.io/gorm"
 	"gorm.io/driver/mysql"
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	gormzerolog "github.com/vitaliy-art/gorm-zerolog"
 )
 
 func Connect() *gorm.DB {
@@ -22,9 +24,22 @@ func Connect() *gorm.DB {
 	db_port := os.Getenv("DB_PORT")
 	db_name := os.Getenv("DB_NAME")
 
+	runLogFile, _ := os.OpenFile(
+		"logs/db.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0664,
+	)
+	multi := zerolog.MultiLevelWriter(os.Stdout, runLogFile)
+	zeroLogger := zerolog.New(multi).With().Timestamp().Logger()
+
+	logger := gormzerolog.NewGormLogger().WithInfo(func() gormzerolog.Event {
+    return &gormzerolog.GormLoggerEvent{Event: zeroLogger.Info()}
+	})
+
   dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_user, db_password, db_host, db_port, db_name)
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger: logger,
 	})
 
 	if err != nil {
