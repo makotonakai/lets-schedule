46049e92a81686598cdd527a93affb97804b00ee
makotonakai <macofandohr1812@gmail.com>
2024-11-17 12:49:31 +0900

    convert all the error messages to error objects

--- src/config/config.go
+++ src/config/config.go
@@ -5,23 +5,24 @@ import (
 )
 
 var (
-	EmailAddressIsEmpty = "メールアドレスが入力されていません"
-	UserNameIsEmpty = "ユーザー名が入力されていません"
-	PasswordIsEmpty = "パスワードが入力されていません"
-	UserAlreadyExists = "ユーザーがすでに登録されています"
-	LoginFailed = "ログインに失敗しました"
-	AvailableTimeNotFound = "ミーティング可能な時間が見つかりません"
-	AvailableTimeTooLong = "入力されたミーティング時間が長すぎます"
-	AvailableTimeOutOfLimit = "入力された時間帯はミーティング可能な時間帯の範囲外です"
-	TitleIsEmpty = "タイトルが入力されていません"
-	HourIsEmpty = "時間が入力されていません"
-	MeetingNotFound = "ミーティングが見つかりません"
-	PlaceIsNotSpecified = "開催場所が指定されていません"
-	URLIsNotSpecified = "URLが指定されていません"
-	NeitherPlaceOrURLIsSpecified = "開催場所もURLも指定されていません"
-	CandidateTimeIsEmpty = "空のミーティング候補時間があります"
-	CandidateTimeIsPast = "過去のミーティング候補時間があります"
+	EmailAddressIsEmpty = errors.New("メールアドレスが入力されていません")
+	UserNameIsEmpty = errors.New("ユーザー名が入力されていません")
+	PasswordIsEmpty = errors.New("パスワードが入力されていません")
+	UserAlreadyExists = errors.New("ユーザーがすでに登録されています")
+	LoginFailed = errors.New("ログインに失敗しました")
+	AvailableTimeNotFound = errors.New("ミーティング可能な時間が見つかりません")
+	AvailableTimeTooLong = errors.New("入力されたミーティング時間が長すぎます")
+	AvailableTimeOutOfLimit = errors.New("入力された時間帯はミーティング可能な時間帯の範囲外です")
+	TitleIsEmpty = errors.New("タイトルが入力されていません")
+	HourIsEmpty = errors.New("時間が入力されていません")
+	MeetingNotFound = errors.New("ミーティングが見つかりません")
+	PlaceIsNotSpecified = errors.New("開催場所が指定されていません")
+	URLIsNotSpecified = errors.New("URLが指定されていません")
+	NeitherPlaceOrURLIsSpecified = errors.New("開催場所もURLも指定されていません")
+	CandidateTimeIsEmpty = errors.New("空のミーティング候補時間があります")
+	CandidateTimeIsPast = errors.New("過去のミーティング候補時間があります")
 	ErrIdConversionFailed = errors.New("The given id is failed to be converted to an integer")
+	ErrFailedToBindUser = errors.New("Failed to bind a new user")
 	ErrUserIsNil = errors.New("The pointer of the given User object is nil")
 	ErrListOfErrorsNotFound = errors.New("The list of error messages doesn't exist")
 	ErrEmailAddressIsEmpty = errors.New("The given email address is empty")
