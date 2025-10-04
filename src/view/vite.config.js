8549065ddae332d113c4edc118fd271414a678a2
makotonakai <macofandohr1812@gmail.com>
2025-10-04 16:48:10 +0900

    Update vite.config.js

--- src/view/vite.config.js
+++ src/view/vite.config.js
@@ -1,29 +1,29 @@
+
 import { fileURLToPath, URL } from 'url'
-import { defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
-  define: {
-    'process.env.HOST': JSON.stringify(env.HOST),
-    'process.env.PORT': JSON.stringify(env.PORT),
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: `${process.env.HOST}:${process.env.PORT}`,
-        changeOrigin: true
+    define: {
+      'process.env.HOST': JSON.stringify(env.HOST),
+      'process.env.PORT': JSON.stringify(env.PORT),
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: `${env.HOST}:${env.PORT}`, // use env directly here
+          changeOrigin: true
+        }
       }
     }
   }
-}
 })
